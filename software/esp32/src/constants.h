9fa9c6acdca5d1aa233e9e4689dae1debe36be55
Javier Fajardo Zafra <jafajard@microsoft.com>
2020-12-30 12:13:38 -0800

    Read BT to move Forward/Backward

--- software/esp32/src/constants.h
+++ software/esp32/src/constants.h
@@ -3,15 +3,26 @@ const int RTS_BAUD_RATE = 115200;
 const char* RTS_BT_NAME = "ShermanToyTank";
 const int RTS_PWM_FREQ  = 5000; // 5KHz
 const int RTS_PWM_RES   = 8;    // 8-bit resolution, 0 to 255
+const int RTS_TIME_DIV  = 80;
+
+const int RTS_WATCHDOG_TIMER = 0;
+const int RTS_WATCHDOG_PERIOD= 33000; // 33ms
 
 // PWM Channel Assignments
-const int TEST_PWM = 0;
-const int RIGHT_MOTOR_CH = 1;
-const int LEFT_MOTOR_CH = 2;
-const int TURRET_CH = 2;
+const uint8_t TEST_PWM = 0;
+const uint8_t RIGHT_MOTOR_CH1 = 1;
+const uint8_t RIGHT_MOTOR_CH2 = 2;
+const uint8_t LEFT_MOTOR_CH1= 3;
+const uint8_t LEFT_MOTOR_CH2= 4;
+const uint8_t TURRET_CH = 5;
 
 // GPIO Assignments
-const int TEST_PWM_PIN = 13;
-const int LEFT_MOTOR_PWN_PIN = 27;
-const int RIGHT_MOTOR_PWN_PIN = 26;
-const int TURRET_PWN_PIN = 25;
+// NOTE: On our dev board, pins 35, 34, 39, 36 seem busted.
+const uint8_t BOOT_BUTTON = 0;
+const uint8_t TEST_PWM_PIN = 33;
+const uint8_t DRV_FAULT_IN_PIN = 26;
+const uint8_t RIGHT_MOTOR_PWN_PIN1 = 27; //AIN1
+const uint8_t RIGHT_MOTOR_PWN_PIN2 = 14; //AIN2
+const uint8_t LEFT_MOTOR_PWN_PIN1 = 12;  //BIN1
+const uint8_t LEFT_MOTOR_PWN_PIN2 = 13;  //BIN2
+const uint8_t TURRET_PWN_PIN = 25; // Currently unused...
--- software/esp32/src/main.cpp
+++ software/esp32/src/main.cpp
@@ -12,12 +12,17 @@ BluetoothSerial SerialBT;
 
 hw_timer_t * timer = NULL;
 
+void Log(const char* msg){
+  Serial.println(msg);
+  SerialBT.println(msg);
+}
+
 void playWithLED(){
   const int min = 0, max = 255;
   static int step = +1;
   static int dutyCycle = 1;
 
-  if(dutyCycle == 255 || dutyCycle == 0){
+  if(dutyCycle == max || dutyCycle == min){
     step *= -1;
   }
 
@@ -30,43 +35,97 @@ void setup() {
   SerialBT.begin(RTS_BT_NAME);
   Serial.println("Device serial started");
 
+  // Motor controller pin in
+  pinMode(DRV_FAULT_IN_PIN, INPUT);
+
   // Setup PWM
-  // configure LED PWM functionalitites
   ledcSetup(TEST_PWM, RTS_PWM_FREQ, RTS_PWM_RES);
+  ledcSetup(RIGHT_MOTOR_CH1, RTS_PWM_FREQ, RTS_PWM_RES);
+  ledcSetup(RIGHT_MOTOR_CH2, RTS_PWM_FREQ, RTS_PWM_RES);
+  ledcSetup(LEFT_MOTOR_CH1, RTS_PWM_FREQ, RTS_PWM_RES);
+  ledcSetup(LEFT_MOTOR_CH2, RTS_PWM_FREQ, RTS_PWM_RES);
   
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(TEST_PWM_PIN, TEST_PWM);
+  ledcAttachPin(RIGHT_MOTOR_PWN_PIN1, RIGHT_MOTOR_CH1);
+  ledcAttachPin(RIGHT_MOTOR_PWN_PIN2, RIGHT_MOTOR_CH2);
+  ledcAttachPin(LEFT_MOTOR_PWN_PIN1, LEFT_MOTOR_CH1);
+  ledcAttachPin(LEFT_MOTOR_PWN_PIN2, LEFT_MOTOR_CH2);
 
   Serial.println("PWM Enabled");
 
-  timer = timerBegin(0, 80000, true);
-  timerAttachInterrupt(timer, &playWithLED, true);
-  timerAlarmWrite(timer, 15, true); // 15 ms
-  timerAlarmEnable(timer);
+  // timer = timerBegin(0, 80, true);
+  // timerAttachInterrupt(timer, &playWithLED, true);
+  // timerAlarmWrite(timer, 15000, true); // 15 ms
+  // timerAlarmEnable(timer);
 
-  Serial.println("Timer Enabled");
+  // Serial.println("Timer Enabled");
+  Serial.println("Device Ready");
 }
 
+void HandleMessage(const char* msg){
+  // Single char message (hopefully)
+  char cmd = msg[0];
+  switch (cmd)
+  {
+  case 'F':
+    Log("Forward");
+    ledcWrite(RIGHT_MOTOR_CH1, 255);
+    ledcWrite(RIGHT_MOTOR_CH2, 0);
+    ledcWrite(LEFT_MOTOR_CH1, 255);
+    ledcWrite(LEFT_MOTOR_CH2, 0);
+    break;
+  case 'B':
+    Log("Back");
+    ledcWrite(RIGHT_MOTOR_CH1, 0);
+    ledcWrite(RIGHT_MOTOR_CH2, 255);
+    ledcWrite(LEFT_MOTOR_CH1, 0);
+    ledcWrite(LEFT_MOTOR_CH2, 255);
+    break;
+  case 'L':
+    Log("LEFT");
+    ledcWrite(RIGHT_MOTOR_CH1, 0);
+    ledcWrite(RIGHT_MOTOR_CH2, 0);
+    ledcWrite(LEFT_MOTOR_CH1, 0);
+    ledcWrite(LEFT_MOTOR_CH2, 0);
+    break;
+  case 'R':
+    Log("RIGHT");
+    ledcWrite(RIGHT_MOTOR_CH1, 0);
+    ledcWrite(RIGHT_MOTOR_CH2, 0);
+    ledcWrite(LEFT_MOTOR_CH1, 0);
+    ledcWrite(LEFT_MOTOR_CH2, 0);
+    break;
+
+  case 'S':
+    Log("STOP");
+    ledcWrite(RIGHT_MOTOR_CH1, 0);
+    ledcWrite(RIGHT_MOTOR_CH2, 0);
+    ledcWrite(LEFT_MOTOR_CH1, 0);
+    ledcWrite(LEFT_MOTOR_CH2, 0);
+
+  default:
+    break;
+  }
+}
+
+// Our DRV8833 has a fault pin. It'll be driven low in the event of an actual fault
+static int DRV_FAULT = -1;
+
 void loop() {
-  // // increase the LED brightness
-  // for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
-  //   // changing the LED brightness with PWM
-  //   ledcWrite(TEST_PWM, dutyCycle);
-  //   delay(15);
-  // }
-
-  // // decrease the LED brightness
-  // for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
-  //   // changing the LED brightness with PWM
-  //   ledcWrite(TEST_PWM, dutyCycle);   
-  //   delay(15);
-  // }
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
   if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
+    // Do the thing
+    String msg = SerialBT.readString();
+    Serial.println(msg);
+    HandleMessage(msg.c_str());
+    
+    // Check DRV State
+    int curr_state = digitalRead(DRV_FAULT_IN_PIN);
+    if(curr_state != DRV_FAULT){
+      Serial.printf("DRV STATE: %s", curr_state?"No issues":"A Fault has ocurred!!!");
+      DRV_FAULT = curr_state;
+    }
   }
-
-  delay(20);
+  
+  // delay(20);
 }
